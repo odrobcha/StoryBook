52c5584ad0bdcff2537099cc24f6d1f739211c5e
odrobcha <drobchak.o@gmail.com>
2024-02-16 11:27:13 +0100

    fix: Display correctly button label

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -14,7 +14,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      New Changes{label}
+      {label}
     </button>
   );
 };
